3d58cd3324c87fafb904c52ff2292b46f08728ac
singh-ml <cs14resch11003@iith.ac.in>
2021-10-20 21:31:56 +0900

    a

--- deep-nys-curve/ray_tune.py
+++ deep-nys-curve/ray_tune.py
@@ -220,4 +220,4 @@ def main(num_samples=10, max_num_epochs=10, gpus_per_trial=2):
 
 if __name__ == "__main__":
     # You can change the number of GPUs per trial here:
-    main(num_samples=10, max_num_epochs=10, gpus_per_trial=1)
+    main(num_samples=2, max_num_epochs=10, gpus_per_trial=1)
